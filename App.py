5e25b4fb4b55f9dbf91470f95538c93a0c331d17
Gloriose-NT <Ntukanyagwegloriose003@gmail.com>
2026-05-04 15:51:49 +0200

    Update App.py

--- App.py
+++ App.py
@@ -1,8 +1,10 @@
 import streamlit as st
 import pandas as pd
-
-
-
+import plotly.express as px
+import plotly.graph_objects as go
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
 import numpy as np
 
 # ── Page Config ─────────────────────────────────────────
