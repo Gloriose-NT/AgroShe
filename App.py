d450b223a673a26596db6bdbce8407dc453fe9b7
Gloriose-NT <Ntukanyagwegloriose003@gmail.com>
2026-05-04 15:49:08 +0200

    Update App.py

--- App.py
+++ App.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
+
 import plotly.graph_objects as go
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
